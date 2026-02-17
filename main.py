fd2a44cb2834f617412975dba7cc734bdc8fd273
vbancheta-nya <vbancheta@carc.pshs.edu.ph>
2026-02-17 13:02:33 +0800

    Added few questions for reference, in hashtags/note

--- main.py
+++ main.py
@@ -9,3 +9,22 @@ Group Members:
 # At your service and ready to lead!
 
 print("Hello World!")
+
+#questions:
+#multiple choice:
+#[ "Imagine if you were at the beach and noticed that a wave hits the shore every 5 seconds. What is the frequency?"
+# a. 5 Hz, b. 0.5 Hz, c. 0.2 Hz, d. 10 Hz
+# correct answer = C ]
+#[ "Which kind of wave does not need a medium to pass through?"
+# a. sound wave, b. light wave, c. water wave, d.seismic waves
+#correct answer = B ]
+
+#true or false
+#[ "TRUE OR FALSE!!"]
+#[ "When a wave does not require a medium, it is considered a Mechanical wave."
+# correct = FALSE, if modified = electromagnetic wave ]
+#["In a wave, period and frequency are inversely proportional.
+# correct = TRUE ]
+#[ "If a wave bends, then it is considered as diffraction."
+# correct = FALSE, if modified = refraction]
+
